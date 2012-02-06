e8942c3a523b90140b3258d2576821c2a16ed6f0
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-02-06 15:42:27 -0500

    Initial commit with base and handler

---
+++ siv/base.py
@@ -0,0 +1,13 @@
+""" 
+Contains a list of utility programs that are used by the rest of the 
+analysis module
+@author: smoitra@cs.cmu.edu
+
+BSD License
+"""
+
+import sys, os
+
+
+	
+
---
+++ siv/handler.py
@@ -0,0 +1,25 @@
+""" 
+Various methods and objects to deal with the SIV sequence
+
+@author: smoitra@cs.cmu.edu 
+BSD License
+
+Definite Features
+* Load the Fasta sequence 
+* Gap strip a sequence 
+* Identify all characters in the alignment
+* Calculate the consensus sequence of the alignment
+
+* Refine the alignment ? 
+* Gap strip the entire alignment ? 
+
+
+"""
+
+class SIVHandler(object) : 
+	""" Defines members and methods for SIV alignment class"""
+	def __init__(self,fas_aln) : 
+		""" Inits the siv class"""
+		pass
+
+	
